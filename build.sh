841bf845989009a7807f6da1ae9ce952310223ff
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2016-10-17 22:49:32 -0400

    Added postgresql to gogs app

--- build.sh
+++ build.sh
@@ -30,7 +30,10 @@ rm -rf build
 BUILD_DIR=${DIR}/build/${NAME}
 mkdir -p ${BUILD_DIR}
 
+DOWNLOAD_URL=http://build.syncloud.org:8111/guestAuth/repository/download
+
 coin --to ${BUILD_DIR} raw https://dl.gogs.io/${GOGS_FILENAME} --takefolder gogs
+coin --to ${BUILD_DIR} raw ${DOWNLOAD_URL}/thirdparty_postgresql_${ARCH}/lastSuccessful/postgresql-${ARCH}.tar.gz
 
 cp -r ${DIR}/bin ${BUILD_DIR}
 cp -r ${DIR}/config ${BUILD_DIR}/config.templates
