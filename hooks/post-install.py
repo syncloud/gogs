dfd8c9bd43cb434060db8180c0e8d392c21f9491
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2016-11-08 23:52:06 -0500

    Initial configuration is done

--- hooks/post-install.py
+++ hooks/post-install.py
@@ -8,6 +8,10 @@ PSQL_PORT = 5433
 DB_USER = 'git'
 DB_PASS = 'git'
 DB_NAME = 'gogs'
+GOG_REPOS_DIR = 'gogs-repositories'
+GOGS_PORT = 3000
+GOGS_ADMIN_USER = 'gogs'
+GOGS_ADMIN_PASSWORD = 'gogs'
 
 from os.path import isdir, join
 import requests
@@ -16,6 +20,18 @@ from subprocess import check_output
 import shutil
 from syncloud_app import logger
 
+from syncloud_platform.application import api
+from syncloud_platform.gaplib import fs, linux, gen
+
+
+def wait_url(url, timeout, interval = 1):
+    t0 = time.clock()
+    while time.clock() - t0 < timeout:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return True
+        time.sleep(interval)
+    return False
 
 def installed(database_path):
     return isdir(database_path)
@@ -50,9 +66,6 @@ class Database:
         log.info(check_output(command_line, shell=True))
 
 
-from syncloud_platform.application import api
-from syncloud_platform.gaplib import fs, linux, gen
-
 log = logger.get_logger('nextcloud_installer')
 
 app = api.get_app_setup(APP_NAME)
@@ -69,6 +82,7 @@ log_path = join(app_data_dir, 'log')
 fs.makepath(log_path)
 
 database_path = join(app_data_dir, PSQL_DATA_PATH)
+gogs_repos_path = join(app_data_dir, GOG_REPOS_DIR)
 
 variables = {
     'app_dir': app_dir,
@@ -77,7 +91,9 @@ variables = {
     'db_psql_port': PSQL_PORT,
     'db_name': DB_NAME,
     'db_user': DB_USER,
-    'db_password': DB_PASS
+    'db_password': DB_PASS,
+    'gogs_repos_path': gogs_repos_path,
+    'log_path': log_path
 }
 
 templates_path = join(app_dir, 'config.templates')
@@ -100,44 +116,48 @@ if first_install:
     db_postgres.execute("CREATE DATABASE {0} WITH OWNER={1};".format(DB_NAME, DB_USER))
 
 app.add_service(SYSTEMD_GOGS)
+app.register_web(GOGS_PORT)
 
-app.register_web(3000)
+app_url = app.app_url()
 
-time.sleep(5)
 
-log.info("Making POST request to finish GOGS installation")
+install_url = 'http://localhost:{}/install'.format(GOGS_PORT)
 
-install_response = requests.post('http://localhost:3000/install', data = {
-    'db_type': 'PostgreSQL',
-    'db_host': '/opt/data/gogs/database:5433',
-    'db_user': 'git',
-    'db_passwd': 'git',
-    'db_name': 'gogs',
-    'ssl_mode': 'disable',
-    'db_path': 'data/gogs.db',
-    'app_name': 'Gogs: Go Git Service',
-    'repo_root_path': '/opt/data/gogs/gogs-repositories',
-    'run_user': 'git',
-    'domain': 'localhost',
-    'ssh_port': '22',
-    'http_port': '3000',
-    'app_url': 'http://localhost:3000/',
-    'log_root_path': '/opt/data/gogs/log',
-    'smtp_host': '',
-    'smtp_from': '',
-    'smtp_email': '',
-    'smtp_passwd': '',
-    'disable_registration': 'on',
-    'require_sign_in_view': 'on',
-    'admin_name': 'gogs',
-    'admin_passwd': 'gogs',
-    'admin_confirm_passwd': 'gogs',
-    'admin_email': 'vladimir.sapronov@gmail.com'
-})
-
-if install_response.status_code != 200:
-    log.error('GOGS finish installation failed with status code: {}', install_response.status_code)
-    log.error('GOGS finish installation POST request response:\n{}', str(install_response))
+if not wait_url(install_url, timeout=5):
+    log.error('Timeout waiting for url: {}', install_url)
+    log.error('Failed to finish GOGS installation')
 else:
-    log.info('GOGS finish installation succeded')
-    
+    log.info("Making POST request to finish GOGS installation, url: {}".format(install_url))
+    install_response = requests.post(install_url, data = {
+        'db_type': 'PostgreSQL',
+        'db_host': '{}:{}'.format(database_path, PSQL_PORT),
+        'db_user': DB_USER,
+        'db_passwd': DB_PASS,
+        'db_name': DB_NAME,
+        'ssl_mode': 'disable',
+        'db_path': 'data/gogs.db',
+        'app_name': 'Gogs: Go Git Service',
+        'repo_root_path': gogs_repos_path,
+        'run_user': USER_NAME,
+        'domain': app_url,
+        'ssh_port': '22',
+        'http_port': str(GOGS_PORT),
+        'app_url': '{}/'.format(app_url),
+        'log_root_path': log_path,
+        'smtp_host': '',
+        'smtp_from': '',
+        'smtp_email': '',
+        'smtp_passwd': '',
+        'disable_registration': 'on',
+        'require_sign_in_view': 'on',
+        'admin_name': GOGS_ADMIN_USER,
+        'admin_passwd': GOGS_ADMIN_PASSWORD,
+        'admin_confirm_passwd': GOGS_ADMIN_PASSWORD,
+        'admin_email': app.redirect_email()
+    })
+
+    if install_response.status_code != 200:
+        log.error('GOGS finish installation failed with status code: {}', install_response.status_code)
+        log.error('GOGS finish installation POST request response:\n{}', str(install_response))
+    else:
+        log.info('GOGS finish installation succeded')
