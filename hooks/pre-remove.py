cd7e03035bdf3d24cf5bd01562202360225adedb
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2016-11-09 21:13:59 -0500

    Moved all hooks code to gogs_hooks module to share some code

--- hooks/pre-remove.py
+++ hooks/pre-remove.py
@@ -1,19 +1,3 @@
-APP_NAME = 'gogs'
-USER_NAME = 'git'
-SYSTEMD_GOGS = 'gogs'
-SYSTEMD_POSTGRESQL = 'gogs-postgresql'
+import gogs_hooks
 
-from syncloud_platform.application import api
-from syncloud_platform.gaplib import fs
-
-app = api.get_app_setup(APP_NAME)
-
-app.unregister_web()
-
-app.remove_service(SYSTEMD_GOGS)
-app.remove_service(SYSTEMD_POSTGRESQL)
-
-app_dir = app.get_install_dir()
-
-fs.removepath(app_dir)
-
+gogs_hooks.remove()
