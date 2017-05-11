9d62fe55502b6a2fdcb12de672b162a0a75d2984
Boris Rybalkin <ribalkin@gmail.com>
2017-05-11 08:50:45 +0100

    v0.11.4

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ export TMP=/tmp
 NAME=gogs
 COIN_CACHE_DIR=${DIR}/coin.cache
 ARCH=$(dpkg-architecture -qDEB_HOST_GNU_CPU)
-GOGS_VERSION=0.9.128
+GOGS_VERSION=0.11.4
 if [ ! -z "$1" ]; then
     ARCH=$1
 fi
@@ -20,13 +20,10 @@ if [ ! -z "$2" ]; then
 fi
 
 if [ "${ARCH}" == 'x86_64' ]; then
-    GOGS_ARCH=amd64
+    GOGS_ARCH=linux_amd64
+else
+    GOGS_ARCH=raspi2_armv6
 fi
-if [ "${ARCH}" == 'armv7l' ]; then
-    GOGS_ARCH=armv6
-fi
-
-GOGS_FILENAME=gogs_v${GOGS_VERSION}_linux_${GOGS_ARCH}.zip
 
 rm -rf build
 BUILD_DIR=${DIR}/build/${NAME}
@@ -34,7 +31,7 @@ mkdir -p ${BUILD_DIR}
 
 DOWNLOAD_URL=http://build.syncloud.org:8111/guestAuth/repository/download
 
-coin --to ${BUILD_DIR} raw https://dl.gogs.io/${GOGS_FILENAME} --takefolder gogs
+coin --to ${BUILD_DIR} raw https://dl.gogs.io/${GOGS_VERSION}/${GOGS_ARCH}.zip --takefolder gogs
 coin --to ${BUILD_DIR} raw ${DOWNLOAD_URL}/thirdparty_postgresql_${ARCH}/lastSuccessful/postgresql-${ARCH}.tar.gz
 coin --to ${BUILD_DIR} --ignore-cache raw ${DOWNLOAD_URL}/thirdparty_git_${ARCH}/lastSuccessful/git-${ARCH}.tar.gz
 
