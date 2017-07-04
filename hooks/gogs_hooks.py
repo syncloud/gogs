7730681d70a1903b6869c54958245ca161444caa
Boris Rybalkin <ribalkin@gmail.com>
2017-07-04 11:14:27 +0100

    debug sys path

--- hooks/gogs_hooks.py
+++ hooks/gogs_hooks.py
@@ -7,7 +7,7 @@ app_path = abspath(join(dirname(__file__), '..'))
 lib_path = join(app_path, 'lib')
 libs = [join(lib_path, item) for item in listdir(lib_path) if isdir(join(lib_path, item))]
 map(sys.path.append, libs)
-
+print(sys.path)
 from bs4 import BeautifulSoup
 
 APP_NAME = 'gogs'
