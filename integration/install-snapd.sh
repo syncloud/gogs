1dd9c620d4bbd2bdecdb9ea18e460b0e39883b36
Boris Rybalkin <ribalkin@gmail.com>
2018-02-04 21:58:50 +0000

    upgrade snapd for user

--- integration/install-snapd.sh
+++ integration/install-snapd.sh
@@ -8,7 +8,7 @@ if [[ -z "$1" ]]; then
 fi
 
 ARCH=$(dpkg --print-architecture)
-VERSION=180201
+VERSION=180204
 
 SNAPD=snapd-${VERSION}-${ARCH}.tar.gz
 wget http://apps.syncloud.org/apps/${SNAPD} --progress=dot:giga
