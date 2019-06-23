a7dfadaf12aca299b0f0d4f23b5cbb1f0d74fe54
Boris Rybalkin <ribalkin@gmail.com>
2019-06-23 10:17:00 +0100

    fix screenshots

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -10,6 +10,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from syncloudlib.integration.screenshots import screenshots
 
 DIR = dirname(__file__)
 screenshot_dir = join(DIR, 'screenshot')
