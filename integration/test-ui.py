eeb7bf45aa16e992ffb6b92f79a79cb8e51f6141
Boris Rybalkin <ribalkin@gmail.com>
2019-06-23 12:45:14 +0100

    ui wait

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -25,7 +25,7 @@ def module_setup(request, device, log_dir, ui_mode):
 def module_teardown(device, log_dir, ui_mode):
     device.activated()
     device.run_ssh('mkdir -p {0}'.format(TMP_DIR), throw=False)
-    device.run_ssh('journalctl > {0}/journalctl.ui.{1} log'.format(TMP_DIR, ui_mode), throw=False)
+    device.run_ssh('journalctl > {0}/journalctl.ui.{1}.log'.format(TMP_DIR, ui_mode), throw=False)
     device.run_ssh('cp /var/log/syslog {0}/syslog.ui.{1}.log'.format(TMP_DIR, ui_mode), throw=False)
       
     device.scp_from_device('{0}/*'.format(TMP_DIR), join(log_dir, 'log'))
@@ -94,18 +94,20 @@ def test_create_repo_empty(app_domain, driver, ui_mode):
 def test_create_repo_init(app_domain, driver, ui_mode):
 
     driver.get("https://{0}/repo/create".format(app_domain))
-    # print(driver.page_source.encode("utf-8"))
-    # time.sleep(5)
     wait_driver = WebDriverWait(driver, 10)
     wait_driver.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.green')))
 
     name = driver.find_element_by_id("repo_name")
     name.send_keys('init')
+    time.sleep(2)
+    screenshots(driver, screenshot_dir, 'repo-create-init-' + ui_mode)
 
+    wait_driver.until(EC.element_to_be_clickable((By.NAME, 'auto_init')))
     auto_init = driver.find_element_by_name("auto_init")
     auto_init.click()
     screenshots(driver, screenshot_dir, 'repo-create-init-' + ui_mode)
 
+    wait_driver.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.green')))
     create = driver.find_element_by_css_selector(".green")
     create.click()
 
