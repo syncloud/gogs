6f9b3a3c4e8c832659aeb38b4b291c06362625e1
Boris Rybalkin <ribalkin@gmail.com>
2022-08-14 08:19:11 +0100

    fix

--- download.sh
+++ download.sh
@@ -17,14 +17,6 @@ wget --progress=dot:giga ${DOWNLOAD_URL}/nginx/nginx-${ARCH}.tar.gz
 tar xf nginx-${ARCH}.tar.gz
 mv nginx ${BUILD_DIR}
 
-wget --progress=dot:giga ${DOWNLOAD_URL}/git-${ARCH}.tar.gz
-tar xf git-${ARCH}.tar.gz
-mv git ${BUILD_DIR}
-
-wget --progress=dot:giga https://github.com/syncloud/3rdparty/releases/download/1/postgresql-${ARCH}.tar.gz
-tar xf postgresql-${ARCH}.tar.gz
-mv postgresql ${BUILD_DIR}
-
 wget https://github.com/gogits/gogs/archive/v${GOGS_VERSION}.tar.gz --progress dot:giga -O gogs-${GOGS_VERSION}.tar.gz
 tar xf gogs-${GOGS_VERSION}.tar.gz
 mv gogs-${GOGS_VERSION} gogs
