ba1b53d8d846eac39eedda24f75b90d6ac1532a8
Boris Rybalkin <ribalkin@gmail.com>
2022-08-18 08:35:20 +0100

    fix

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -8,23 +8,21 @@ from syncloudlib.integration.hosts import add_host_alias
 from integration import lib
 
 DIR = dirname(__file__)
-screenshot_dir = join(DIR, 'screenshot')
-TMP_DIR = '/tmp/syncloud/ui'
 
 
 @pytest.fixture(scope="session")
-def module_setup(request, device, log_dir, ui_mode):
-    request.addfinalizer(lambda: module_teardown(device, log_dir, ui_mode))
-
-
-def module_teardown(device, log_dir, ui_mode):
-    device.activated()
-    device.run_ssh('mkdir -p {0}'.format(TMP_DIR), throw=False)
-    device.run_ssh('journalctl > {0}/journalctl.ui.{1}.log'.format(TMP_DIR, ui_mode), throw=False)
-    device.run_ssh('cp /var/log/syslog {0}/syslog.ui.{1}.log'.format(TMP_DIR, ui_mode), throw=False)
+def module_setup(request, device, log_dir, ui_mode, artifact_dir):
+    def module_teardown():
+        tmp_dir = '/tmp/syncloud/ui'
+        device.activated()
+        device.run_ssh('mkdir -p {0}'.format(tmp_dir), throw=False)
+        device.run_ssh('journalctl > {0}/journalctl.ui.{1}.log'.format(tmp_dir, ui_mode), throw=False)
+        device.run_ssh('cp /var/log/syslog {0}/syslog.ui.{1}.log'.format(tmp_dir, ui_mode), throw=False)
       
-    device.scp_from_device('{0}/*'.format(TMP_DIR), join(log_dir, 'log'))
+        device.scp_from_device('{0}/*'.format(tmp_dir), artifact_dir)
+        check_output('chmod -R a+r {0}'.format(artifact_dir), shell=True)
 
+    request.addfinalizer(module_teardown)
 
 def test_start(module_setup, app, domain, device_host):
     add_host_alias(app, device_host, domain)
