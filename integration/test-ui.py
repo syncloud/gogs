b0e4c711e89747c144a2e7aca6af47fc429ee5ca
Boris Rybalkin <ribalkin@gmail.com>
2022-08-19 22:06:16 +0100

    fix

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -15,7 +15,7 @@ def module_setup(request, device, log_dir, ui_mode, artifact_dir):
     def module_teardown():
         tmp_dir = '/tmp/syncloud/ui'
         device.activated()
-        device.run_ssh('mkdir -p {0}'.format(tmp_dir), throw=False)
+        device.run_ssh('mkdir -p {0}/{1}'.format(tmp_dir, ui_mode), throw=False)
         device.run_ssh('journalctl > {0}/{1}/journalctl.log'.format(tmp_dir, ui_mode), throw=False)
         device.scp_from_device('{0}/*'.format(tmp_dir), artifact_dir)
         check_output('chmod -R a+r {0}'.format(artifact_dir), shell=True)
