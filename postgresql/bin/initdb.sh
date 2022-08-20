821290ca32099df0fce022af80d085f1f34c234b
Boris Rybalkin <ribalkin@gmail.com>
2022-08-20 18:48:24 +0100

    lang

--- postgresql/bin/initdb.sh
+++ postgresql/bin/initdb.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
 LIBS=$(echo ${DIR}/lib/*linux*/)
