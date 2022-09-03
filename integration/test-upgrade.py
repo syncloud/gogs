084ccc158a1abf95976bd516c05207824436ea20
Boris Rybalkin <ribalkin@gmail.com>
2022-09-03 21:56:27 +0100

    manual first backup for db upgrade

--- integration/test-upgrade.py
+++ integration/test-upgrade.py
@@ -29,6 +29,10 @@ def test_start(module_setup, app, device_host, domain, device):
 def test_upgrade(device, device_user, device_password, device_host, app_archive_path, app_domain):
     device.run_ssh('snap remove gogs')
     device.run_ssh('snap install gogs')
+
+    # TODO: remove after release of gogs with backup on pre-refresh
+    device.run_ssh('snap run gogs.pgdumpall -f /var/snap/gogs/current/database.dump')
+
     local_install(device_host, device_password, app_archive_path)
     wait_for_rest(requests.session(), "https://{0}".format(app_domain), 200, 10)
 
