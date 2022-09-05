129f8d0f21a4ef4ad00c5a9c7a74379a492ff469
Boris Rybalkin <ribalkin@gmail.com>
2022-09-05 23:03:50 +0100

    fix psql

--- bin/psql.sh
+++ bin/psql.sh
@@ -6,4 +6,4 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 # shellcheck source=config/env
 . "/var/snap/gogs/current/config/env"
 
-${DIR}/postgresql/bin/psql.sh ${PSQL_PORT} -h ${PSQL_DATABASE} "$@"
+${DIR}/postgresql/bin/psql.sh -p ${PSQL_PORT} -h ${PSQL_DATABASE} "$@"
