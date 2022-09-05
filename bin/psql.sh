40336ea50a4e606f52a7319d9f3316e75bf48b6c
Boris Rybalkin <ribalkin@gmail.com>
2022-09-05 09:09:58 +0100

    psql user

--- bin/psql.sh
+++ bin/psql.sh
@@ -6,9 +6,4 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 # shellcheck source=config/env
 . "/var/snap/gogs/current/config/env"
 
-if [[ "$(whoami)" == "git" ]]; then
-    ${DIR}/postgresql/bin/psql.sh -p 5433 -h /var/snap/gogs/common/database "$@"
-else
-    sudo -E -H -u git ${DIR}/postgresql/bin/psql.sh -p 5433 -h /var/snap/gogs/common/database "$@"
-fi
-
+${DIR}/postgresql/bin/psql.sh -p 5433 -U git -h /var/snap/gogs/common/database "$@"
