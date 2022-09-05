e4fe28d7f92ecdb8f369b4556304a16d0e9aa48b
Boris Rybalkin <ribalkin@gmail.com>
2022-09-05 20:28:22 +0100

    pg alpine

--- postgresql/build.sh
+++ postgresql/build.sh
@@ -13,7 +13,7 @@ BUILD_DIR=${DIR}/../build/snap/postgresql
 
 docker ps -a -q --filter ancestor=postgres:syncloud --format="{{.ID}}" | xargs docker stop | xargs docker rm || true
 docker rmi postgres:syncloud || true
-docker build --build-arg MAJOR_VERSION=${MAJOR_VERSION} -t postgres:syncloud .
+docker build --build-arg MAJOR_VERSION=${MAJOR_VERSION}-alpine -t postgres:syncloud .
 docker run postgres:syncloud postgres --help
 docker create --name=postgres postgres:syncloud
 mkdir -p ${BUILD_DIR}
