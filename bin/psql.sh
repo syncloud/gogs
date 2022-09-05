7d57288af32b435f5d209a6c4564807c9d0ac3b5
Boris Rybalkin <ribalkin@gmail.com>
2022-09-05 23:32:31 +0100

    fix psql user again

--- bin/psql.sh
+++ bin/psql.sh
@@ -6,4 +6,4 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 # shellcheck source=config/env
 . "/var/snap/gogs/current/config/env"
 
-${DIR}/postgresql/bin/psql.sh -u git -p ${PSQL_PORT} -h ${PSQL_DATABASE} "$@"
+${DIR}/postgresql/bin/psql.sh -U git -p ${PSQL_PORT} -h ${PSQL_DATABASE} "$@"
