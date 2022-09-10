29512e71b733b47c0186904bdb1603ad5c5e7945
Boris Rybalkin <ribalkin@gmail.com>
2022-09-11 00:38:12 +0100

    git cli test

--- integration/test-upgrade.py
+++ integration/test-upgrade.py
@@ -30,9 +30,8 @@ def test_start(module_setup, app, device_host, domain, device):
 def test_upgrade(device, device_user, device_password, device_host, app_archive_path, app_domain):
     device.run_ssh('snap remove gogs')
     device.run_ssh('snap install gogs')
-
-    # TODO: remove after release of gogs with backup on pre-refresh
-    device.run_ssh('sudo -E -H -u git /snap/gogs/current/postgresql/bin/pg_dumpall.sh -p 5433 -h /var/snap/gogs/common/database -f /var/snap/gogs/current/database.dump')
+    device.run_ssh("git clone https://{0}:{1}@{2}/{3}/init init".format(device_user, device_password, app_domain, device_user))
+    device.run_ssh("cd init; touch 1; git add .; git commit -am 'test'; git push;")
 
     local_install(device_host, device_password, app_archive_path)
     wait_for_rest(requests.session(), "https://{0}".format(app_domain), 200, 10)
