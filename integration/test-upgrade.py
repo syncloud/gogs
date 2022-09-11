601c2553877cde180ba1c4a85fa5b819138eb75b
Boris Rybalkin <ribalkin@gmail.com>
2022-09-11 02:08:28 +0100

    log error

--- integration/test-upgrade.py
+++ integration/test-upgrade.py
@@ -1,5 +1,5 @@
 import pytest
-from subprocess import check_output
+from subprocess import check_output, CalledProcessError, STDOUT
 from syncloudlib.integration.hosts import add_host_alias
 from integration import lib
 from syncloudlib.integration.installer import local_install, wait_for_installer
@@ -14,8 +14,10 @@ def module_setup(request, device, artifact_dir):
         device.run_ssh('journalctl > {0}/refresh.journalctl.log'.format(TMP_DIR), throw=False)
         device.run_ssh('cp /var/snap/gogs/current/database.dump {0}'.format(TMP_DIR), throw=False)
         device.scp_from_device('{0}/*'.format(TMP_DIR), artifact_dir)
-        check_output('cp /videos/* {0}'.format(artifact_dir), shell=True)
-        check_output('chmod -R a+r {0}'.format(artifact_dir), shell=True)
+        try:
+            check_output('cp /videos/* {0}'.format(artifact_dir), shell=True)
+            check_output('chmod -R a+r {0}'.format(artifact_dir), shell=True)
+        except
 
     request.addfinalizer(module_teardown)
 
@@ -30,8 +32,8 @@ def test_start(module_setup, app, device_host, domain, device):
 def test_upgrade(device, device_user, device_password, device_host, app_archive_path, app_domain):
     device.run_ssh('snap remove gogs')
     device.run_ssh('snap install gogs')
-    check_output("git clone https://{0}:{1}@{2}/{3}/init init".format(device_user, device_password, app_domain, device_user), shell=True)
-    check_output("cd init; touch 1; git add .; git commit -am 'test'; git push;", shell=True)
+    run("git clone https://{0}:{1}@{2}/{3}/init init".format(device_user, device_password, app_domain, device_user))
+    run("cd init; touch 1; git add .; git commit -am 'test'; git push;")
 
     local_install(device_host, device_password, app_archive_path)
     wait_for_rest(requests.session(), "https://{0}".format(app_domain), 200, 10)
@@ -39,3 +41,12 @@ def test_upgrade(device, device_user, device_password, device_host, app_archive_
 
 def test_login(selenium, device_user, device_password):
     lib.login(selenium, device_user, device_password)
+
+
+def run(cmd):
+    try:
+        output = check_output(cmd, stderr=STDOUT, shell=True).decode()
+        print(output)
+    except CalledProcessError as e:
+        print("error: " + e.output.decode())
+        raise e
