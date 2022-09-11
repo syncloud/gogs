94c5e0f58a7e26bb1e7d621223ad770318a54ea5
Boris Rybalkin <ribalkin@gmail.com>
2022-09-11 01:26:34 +0100

    fix git test

--- integration/test-upgrade.py
+++ integration/test-upgrade.py
@@ -30,8 +30,8 @@ def test_start(module_setup, app, device_host, domain, device):
 def test_upgrade(device, device_user, device_password, device_host, app_archive_path, app_domain):
     device.run_ssh('snap remove gogs')
     device.run_ssh('snap install gogs')
-    device.run_ssh("git clone https://{0}:{1}@{2}/{3}/init init".format(device_user, device_password, app_domain, device_user))
-    device.run_ssh("cd init; touch 1; git add .; git commit -am 'test'; git push;")
+    check_output("git clone https://{0}:{1}@{2}/{3}/init init".format(device_user, device_password, app_domain, device_user), shell=True)
+    check_output("cd init; touch 1; git add .; git commit -am 'test'; git push;", shell=True)
 
     local_install(device_host, device_password, app_archive_path)
     wait_for_rest(requests.session(), "https://{0}".format(app_domain), 200, 10)
