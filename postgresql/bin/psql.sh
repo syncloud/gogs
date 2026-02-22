f42a48178dacbade29756e06038d57438c35e8eb
Boris Rybalkin <ribalkin@gmail.com>
2026-02-22 09:48:11 +0000

    fix: use bundled ld-linux directly in postgres wrappers for OS isolation

--- postgresql/bin/psql.sh
+++ postgresql/bin/psql.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-LIBS=$(echo ${DIR}/lib/*linux*/)
-LIBS=$LIBS:$(echo ${DIR}/usr/lib/*linux*)
-export LD_LIBRARY_PATH=$LIBS
-exec ${DIR}/usr/lib/postgresql/*/bin/psql "$@"
+LIBS=$(echo ${DIR}/lib/*-linux-gnu*)
+LIBS=$LIBS:$(echo ${DIR}/usr/lib/*-linux-gnu*)
+exec ${DIR}/lib/*-linux*/ld-*.so --library-path $LIBS ${DIR}/usr/lib/postgresql/*/bin/psql "$@"
